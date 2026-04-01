5fd019d41fdbb5626c470c947b95a0e305cc70d7
GuadalupeSoria <soriaguada01@gmail.com>
2026-04-01 18:23:19 +0200

    feat: agregar polyfill para Promise.withResolvers en pdfjs-dist para compatibilidad con navegadores más antiguos

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Polyfill Promise.withResolvers for pdfjs-dist (requires ES2024, missing in older browsers)
+if (typeof (Promise as any).withResolvers === 'undefined') {
+  ;(Promise as any).withResolvers = function <T>() {
+    let resolve!: (value: T | PromiseLike<T>) => void
+    let reject!: (reason?: unknown) => void
+    const promise = new Promise<T>((res, rej) => { resolve = res; reject = rej })
+    return { promise, resolve, reject }
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
